b0d8b0fc2246ac895191600aeabbc02870229ee9
Indira Borras <borrasindira@gmail.com>
2022-12-21 01:49:03 +0000

    user can now add images to campaigns too

--- console.py
+++ console.py
@@ -25,9 +25,9 @@ import repositories.player_history_repository as player_history_repository
 # player_repository.save(player_6)
 
 
-# campaign_1 = Campaign("Quest for Kaladesh", "Adventure", "Steve", 5, 15, "inspired in a long-time classic", "https://static.wikia.nocookie.net/mtgsalvation_gamepedia/images/2/29/Kaladesh_art.jpg/revision/latest?cb=20200831001906")
+# campaign_1 = Campaign("Quest for Kaladesh", "Adventure", "Steve", 5, 15, "inspired in a long-time classic", "https://static.wikia.nocookie.net/mtgsalvation_gamepedia/images/2/29/Kaladesh_art.jpg")
 # campaign_2 = Campaign("Eberron's collapse", "Fanasty", "Octavio", 6, 20, "inspired in D&D", "https://flyinglizardstudio.com/wp-content/uploads/2018/02/ToA-E2.jpg")
-# campaign_3 = Campaign("God save the... Djin(?)", "Fantasy", "Sophie", 4, 10, "unnexpected twists", "https://static.wikia.nocookie.net/mana/images/7/79/Jinn_%28Children_of_Mana%29.png/revision/latest?cb=20090609163529")
+# campaign_3 = Campaign("God save the... Djin(?)", "Fantasy", "Sophie", 4, 10, "unnexpected twists", "https://static.wikia.nocookie.net/mana/images/7/79/Jinn_%28Children_of_Mana%29.png")
 # campaign_4 = Campaign("Zombies, Why not", "apocaliptic", "Steve", 8, 20, "it's fun!", "https://images.fineartamerica.com/images/artworkimages/mediumlarge/3/2-zombies-eat-brains-dont-worry-youre-safe-funny-the-perfect-presents.jpg")
 # campaign_5 = Campaign("Who killed father Toad?", "mystery", "Eva", 6, 15, "if you like frogs and mistery, this is for you", "https://i.etsystatic.com/23664421/r/il/f86f67/4247540905/il_570xN.4247540905_5y7j.jpg")
 # campaign_repository.save(campaign_1)
